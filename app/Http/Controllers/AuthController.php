e986219501abfbcaa12c91d07c807371a76aa0a9
tulbadex <ibrahimadedayo@rocketmail.com>
2021-09-21 19:41:24 -0700

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,5 +18,65 @@ class AuthController extends Controller
             'password' => 'required|string|min:8'
         ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'Error' => $validator->errors()
+            ], 401);
+        }
+        /* $data = $request->validate([
+            'name' => 'required|string|max:50',
+            'email' => 'required|string|email|max:150|unique:users',
+            'password' => 'required|string|min:8'
+        ]); */
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $fields = $request->validate([
+                'email' => 'email|required|string',
+                'password' => 'required|string'
+            ]);
+
+            $user = User::where('email', $fields['email'])->first();
+
+            if (!$user || !Hash::check($fields['password'], $user->password)) {
+                return response()->json([
+                    'message' => ['Credential invalid']
+                ], 401);
+            }
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer'
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Error in login',
+                'error' => $th
+            ], 500);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+                'message' => 'Logged out Successful',
+            ], 201);
     }
 }
